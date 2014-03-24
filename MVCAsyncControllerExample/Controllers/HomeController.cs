e2638458c078d0f47243f75f2dad6dbd14defcb4
scottmhill <scottmwhill@gmail.com>
2014-03-24 15:21:01 -0500

    initial check-in

---
+++ MVCAsyncControllerExample/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAsyncControllerExample.Controllers
+{
+    public class HomeController : AsyncController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void getDataAsync(string term)
+        {
+            AsyncManager.OutstandingOperations.Increment();
+            Fetcher fetcher = new Fetcher();
+            fetcher.fetcherCompleted += (sender, e) =>
+            {
+                AsyncManager.Parameters["json"] = e.jsonData;
+                AsyncManager.OutstandingOperations.Decrement();
+            };
+            fetcher.getJSONDataAsync(term);
+        }
+
+        public ActionResult getDataCompleted(string jsonarg)
+        {
+            //string json = jsona[0];
+            string json = AsyncManager.Parameters["json"].ToString();
+            ContentResult result = Content(json, "application/json");
+
+            return result;
+        }
+    }
+
+    public class Fetcher
+    {
+        public delegate void FetcherCompleted(object sender, FetcherEventArgs e);
+        public FetcherCompleted fetcherCompleted;
+
+        public async void getJSONDataAsync(string term)
+        {
+            HttpClient client = new HttpClient(new HttpClientHandler() { AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip });
+            string url = string.Format("http://api.stackexchange.com/2.2/search?order=desc&sort=activity&site=stackoverflow&intitle={0}", term);
+            var uri = new Uri(url);
+            string json = "";
+
+            await client.GetAsync(uri).ContinueWith(async t =>
+            {
+                json = await t.Result.Content.ReadAsStringAsync();
+            });
+            FetcherEventArgs e = new FetcherEventArgs() { jsonData = json };
+
+            fetcherCompleted(this, e);
+            //fetcherCompleted.Invoke(this, e);
+        }
+    }
+
+    public class FetcherEventArgs : EventArgs
+    {
+        public string jsonData { get; set; }
+    }
+}
---
+++ MVCAsyncControllerExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCAsyncControllerExample.MvcApplication" Language="C#" %>
